7e3be273ef0502224fb5ba1b7efca6b3e605078c
William Hatanaka(MiniDeskH470) <william.ariano@gmail.com>
2024-03-08 23:43:54 +0900

    feat: integracao com laravel-gates

--- tests/Feature/PermissionUserTest.php
+++ tests/Feature/PermissionUserTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\User;
+use Illuminate\Support\Facades\Route;
 
 it('should be able to give a permission to a user', function () {
     // Arrange
@@ -14,4 +15,23 @@ it('should be able to give a permission to a user', function () {
     $this->assertDatabaseHas('permissions', [
         'permission' => 'search-candidates',
     ]);
+});
+
+it('should be able to authorize access to a route based on the permission', function () {
+    // Define a route
+    Route::get('test-something', function () {
+        return 'test';
+    })->middleware('permission:search-candidates');
+
+    $user = User::factory()->createOne();
+
+    // Test access to a route WITHOUT permission
+    $responseWithoutPermission = test()->actingAs($user)->get('test-something');
+    expect($responseWithoutPermission->status())->toBe(403);
+
+    $user->givePermissionTo('search-candidates');
+
+    // Test access to a route WITH permission
+    $responseWithPermission = test()->actingAs($user)->get('test-something');
+    expect($responseWithPermission->status())->toBe(200);
 });
