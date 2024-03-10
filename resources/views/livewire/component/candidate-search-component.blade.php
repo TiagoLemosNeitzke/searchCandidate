cbbeef94fab3184d83b4c5ed7ebb68ec6a4eccdb
tiago <tl.neitzke@hotmail.com>
2024-03-10 10:30:44 -0400

    rf: componentizando

--- resources/views/livewire/component/candidate-search-component.blade.php
+++ resources/views/livewire/component/candidate-search-component.blade.php
@@ -1,95 +1,34 @@
 <div class="w-10/12 mx-auto">
-    <x-error-alert/>
-    <div class="mx-auto flex flex-col lg:flex-row justify-between">
-        <!-- Component: Rounded base basic select -->
-        <div class="relative my-6 md:w-60">
-            <select id="language" wire:model="language" required
-                    class="relative w-full h-10 px-4 text-sm transition-all bg-white border rounded outline-none appearance-none focus-visible:outline-none peer border-slate-200 text-slate-500 autofill:bg-white focus:border-emerald-500 focus:focus-visible:outline-none disabled:cursor-not-allowed disabled:bg-slate-50 disabled:text-slate-400">
-                <option value="" disabled selected></option>
-                <option value="php">PHP</option>
-                <option value="javascript">Javascript</option>
-                <option value="python">Python</option>
-                <option value="golang">Golang</option>
-                <option value="rust">Rust</option>
-                <option value="dart">Dart</option>
-            </select>
-            <label for="language"
-                   class="pointer-events-none absolute top-2.5 left-2 z-[1] px-2 text-sm text-slate-400 transition-all before:absolute before:top-0 before:left-0 before:z-[-1] before:block before:h-full before:w-full before:bg-white before:transition-all peer-required:after:text-pink-500 peer-required:after:content-['\00a0*'] peer-valid:-top-2 peer-valid:text-xs peer-focus:-top-2 peer-focus:text-xs peer-focus:text-emerald-500 peer-disabled:cursor-not-allowed peer-disabled:text-slate-400 peer-disabled:before:bg-transparent">
-                Selecione a linguagem
-            </label>
-            <svg xmlns="http://www.w3.org/2000/svg"
-                 class="pointer-events-none absolute top-2.5 right-2 h-5 w-5 fill-slate-400 transition-all peer-focus:fill-emerald-500 peer-disabled:cursor-not-allowed"
-                 viewBox="0 0 20 20" fill="currentColor" aria-labelledby="title-04 description-04"
-                 role="graphics-symbol">
-                <title id="title-04">Arrow Icon</title>
-                <desc id="description-04">Arrow icon of the select list.</desc>
-                <path fill-rule="evenodd"
-                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                      clip-rule="evenodd"/>
-            </svg>
-        </div>
-        <!-- End Rounded base basic select -->
-        <!-- Component: Rounded base basic select -->
-        <div class="relative my-6 md:w-60">
-            <select id="location" wire:model="location" required
-                    class="relative w-full h-10 px-4 text-sm transition-all bg-white border rounded outline-none appearance-none focus-visible:outline-none peer border-slate-200 text-slate-500 autofill:bg-white focus:border-emerald-500 focus:focus-visible:outline-none disabled:cursor-not-allowed disabled:bg-slate-50 disabled:text-slate-400">
-                <option value="" disabled selected></option>
-                <option value="brasil">Brasil</option>
-                <option value="canada">Canadá</option>
-                <option value="china">China</option>
-            </select>
-            <label for="location"
-                   class="pointer-events-none absolute top-2.5 left-2 z-[1] px-2 text-sm text-slate-400 transition-all before:absolute before:top-0 before:left-0 before:z-[-1] before:block before:h-full before:w-full before:bg-white before:transition-all peer-required:after:text-pink-500 peer-required:after:content-['\00a0*'] peer-valid:-top-2 peer-valid:text-xs peer-focus:-top-2 peer-focus:text-xs peer-focus:text-emerald-500 peer-disabled:cursor-not-allowed peer-disabled:text-slate-400 peer-disabled:before:bg-transparent">
-                Selecione o País
-            </label>
-            <svg xmlns="http://www.w3.org/2000/svg"
-                 class="pointer-events-none absolute top-2.5 right-2 h-5 w-5 fill-slate-400 transition-all peer-focus:fill-emerald-500 peer-disabled:cursor-not-allowed"
-                 viewBox="0 0 20 20" fill="currentColor" aria-labelledby="title-04 description-04"
-                 role="graphics-symbol">
-                <title id="title-04">Arrow Icon</title>
-                <desc id="description-04">Arrow icon of the select list.</desc>
-                <path fill-rule="evenodd"
-                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                      clip-rule="evenodd"/>
-            </svg>
-        </div>
-        <!-- End Rounded base basic select -->
-        <!-- Component: Rounded base basic select -->
-        <div class="relative my-6 md:w-60">
-            <select id="repos" wire:model="repos" required
-                    class="relative w-full h-10 px-4 text-sm transition-all bg-white border rounded outline-none appearance-none focus-visible:outline-none peer border-slate-200 text-slate-500 autofill:bg-white focus:border-emerald-500 focus:focus-visible:outline-none disabled:cursor-not-allowed disabled:bg-slate-50 disabled:text-slate-400">
-                <option value="" disabled selected></option>
-                <option value="10">10</option>
-                <option value="50">50</option>
-                <option value="100">100</option>
-                <option value="200">200</option>
-            </select>
-            <label for="repos"
-                   class="pointer-events-none absolute top-2.5 left-2 z-[1] px-2 text-sm text-slate-400 transition-all before:absolute before:top-0 before:left-0 before:z-[-1] before:block before:h-full before:w-full before:bg-white before:transition-all peer-required:after:text-pink-500 peer-required:after:content-['\00a0*'] peer-valid:-top-2 peer-valid:text-xs peer-focus:-top-2 peer-focus:text-xs peer-focus:text-emerald-500 peer-disabled:cursor-not-allowed peer-disabled:text-slate-400 peer-disabled:before:bg-transparent">
-                Quantidade Repos Públicos
-            </label>
-            <svg xmlns="http://www.w3.org/2000/svg"
-                 class="pointer-events-none absolute top-2.5 right-2 h-5 w-5 fill-slate-400 transition-all peer-focus:fill-emerald-500 peer-disabled:cursor-not-allowed"
-                 viewBox="0 0 20 20" fill="currentColor" aria-labelledby="title-04 description-04"
-                 role="graphics-symbol">
-                <title id="title-04">Arrow Icon</title>
-                <desc id="description-04">Arrow icon of the select list.</desc>
-                <path fill-rule="evenodd"
-                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                      clip-rule="evenodd"/>
-            </svg>
-        </div>
-        <!-- End Rounded base basic select -->
-        <div class="w-2/12 flex space-x-2 items-center">
-            <x-primary-button wire:click="search" class="w-24 h-10">Buscar</x-primary-button>
-            <x-secondary-button wire:click="clear" class="w-24 h-10">Reset</x-secondary-button>
-        </div>
-    </div>
-    <div class="z-50 text-white mb-4" wire:loading>
+    <div class="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 text-white" wire:loading>
         <button type="button" class="bg-indigo-500 p-4 rounded-md" disabled>
             Processing...
         </button>
     </div>
+
+    <x-error-alert/>
+
+    <div class="mx-auto flex flex-col lg:flex-row justify-between">
+        <x-select-component :options="['PHP', 'javascript','Python', 'Golang', 'Rust', 'Dart']"
+                            label="Selecione a linguagem"/>
+
+
+        <div class="relative my-6 md:w-60">
+            <x-text-input class="bg-white" wire:model="location"></x-text-input>
+            <label for="location"
+                   class="pointer-events-none absolute top-2.5 left-2 z-[1] px-2 text-sm text-slate-400 transition-all before:absolute before:top-0 before:left-0 before:z-[-1] before:block before:h-full before:w-full before:bg-white before:transition-all peer-required:after:text-pink-500 peer-required:after:content-['\00a0*'] peer-valid:-top-2 peer-valid:text-xs peer-focus:-top-2 peer-focus:text-xs peer-focus:text-emerald-500 peer-disabled:cursor-not-allowed peer-disabled:text-slate-400 peer-disabled:before:bg-transparent">
+                Escolha um País
+            </label>
+        </div>
+
+        <x-select-component :options="['10', '100', '200', '500', '1000']" label="Quantidade de Repos"/>
+
+        <div class="w-2/12 flex space-x-2 items-center">
+            <x-primary-button wire:click="search" wire:loading.attr="disabled" class="w-24 h-10">Buscar
+            </x-primary-button>
+            <x-secondary-button wire:click="clear" class="w-24 h-10">Reset</x-secondary-button>
+        </div>
+    </div>
+
     {{--table--}}
     <div>
         <div
