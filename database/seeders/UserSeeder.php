64a3f6fd7c4f01784945c1b7cd24afe75631fedf
Elivandro <Elivandro@hotmail.com.br>
2024-03-12 06:45:52 -0400

    feat: userFactory, candidateFactory, seeder e migração

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::factory(1)
+            ->hasCandidates(3)
+            ->create();
+    }
+}
