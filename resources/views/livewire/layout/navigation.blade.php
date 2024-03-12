d9c60552161a961c6c6177cdbea7fab7ecdec70d
tiago <tl.neitzke@hotmail.com>
2024-03-12 19:23:27 -0400

    resolvendo conflitos

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -32,14 +32,6 @@ new class extends Component {
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('candidate.search')" :active="request()->routeIs('candidate.search')" wire:navigate>
-                        {{ __('Buscar Candidato') }}
-                    </x-nav-link>
-                </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('user_permissions')" :active="request()->routeIs('user_permissions')" wire:navigate>
-                        {{ __('Permissao de usuarios') }}
-                    </x-nav-link>
                 </div>
             </div>
 
