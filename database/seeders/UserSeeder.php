549e4778caef9a2528cb360cd0fff9c8641a9b7a
tiago <tl.neitzke@hotmail.com>
2024-03-13 14:50:02 -0400

    rf: retirado a criação de candidatos

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,29 +3,18 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
-
-        User::factory(1)
-            ->hasCandidates(3)
-            ->create();
-
         $user = User::factory()->create([
-            'name' => 'Administrador',
-            'email' => 'admin@admin.com',
-        ]);
-        $user->givePermissionTo('admin');
-
-             $user = User::factory()->create([
             'name' => 'CTO',
             'email' => 'cto@cto.com',
         ]);
         $user->givePermissionTo('searcher');
+        $user->givePermissionTo('admin');
         $user->givePermissionTo('searcher-nominator');
 
         $user = User::factory()->create([
